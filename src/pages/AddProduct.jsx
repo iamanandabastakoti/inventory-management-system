35aaa48b66095b3ebda400d5336e440a71a1727e
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2024-03-28 12:38:36 +0545

    page-not-found config file for vercel

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -30,18 +30,18 @@ const AddProduct = () => {
     }
     return (
         <div className='flex justify-center w-full'>
-            <form className='w-2/5 flex flex-col gap-4 p-4' onSubmit={addProduct}>
+            <form className='w-2/5 flex flex-col gap-4 p-1' onSubmit={addProduct}>
                 <div className='flex flex-col gap-1'>
                     <label htmlFor="name">Name</label>
                     <input className='border border-buttonBg rounded-lg w-full p-2 focus:outline-none' type="text" name='name' placeholder='Enter the name of the product' required onChange={handleChange} />
                 </div>
                 <div className='flex flex-col gap-1'>
                     <label htmlFor="description">Description</label>
-                    <textarea className='border border-buttonBg rounded-lg w-full p-2 resize-none focus:outline-none' name="description" id="" cols="30" rows="5" placeholder="Enter the product's description" required onChange={handleChange}></textarea>
+                    <textarea className='border border-buttonBg rounded-lg w-full p-2 resize-none focus:outline-none' name="description" cols="30" rows="5" placeholder="Enter the product's description" required onChange={handleChange}></textarea>
                 </div>
                 <div>
                     <label htmlFor="image">Image</label>
-                    <input className='border border-buttonBg rounded-lg w-full p-2 focus:outline-none' type="text" name='image' placeholder='Enter the image url of the product' required onChange={handleChange} />
+                    <textarea className='border border-buttonBg rounded-lg w-full p-2 resize-none focus:outline-none' type="text" name='image' cols="30" rows="3" placeholder='Enter the image url of the product' required onChange={handleChange} ></textarea>
                 </div>
                 <div>
                     <label htmlFor="category">Category</label>
